3ed01f5b76c528d66ea59527cea71f2046fa58c2
unknown <saumyadcorehost@gmail.com>
2025-09-26 16:05:28 +0530

    Done FAQ  for all pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
+import FaqSection from '../components/FaqSection/FaqSection'
+import { wifiServiceFaqData } from '../assets/Data/faq'
+
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <FaqSection {...wifiServiceFaqData} />
+      <Footer/>
     </div>
   )
 }
